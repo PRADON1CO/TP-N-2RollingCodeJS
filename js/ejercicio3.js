a33e0d6b08b0e24b4244c51bd060ffaf6df06416
Prado Nicolas <nicolas88prado@gmail.com>
2024-01-13 18:46:43 -0300

    Realice el ejercicio N3 del TP N°2  de js

---
+++ js/ejercicio3.js
@@ -0,0 +1,24 @@
+let oracion = [];
+
+while (true){
+  let oracionNueva = prompt('Introduce una cadena de texto:');
+
+  if(oracionNueva === null) {
+    break;
+  }
+
+  oracion.push(oracionNueva);
+
+  let confirmar = confirm('¿Deseas ingresar otra cadena?');
+
+  if(!confirmar){
+    break;
+  }
+}
+
+if (oracion.length > 0) {
+  let union = oracion.join(' - ');
+  alert("Cadenas concatenadas: " + union);
+} else {
+  alert("No se ingresaron cadenas.");
+}
