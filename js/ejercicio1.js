6df2da901561b5ec7f5b42b8ca5ad012e6fed699
Prado Nicolas <nicolas88prado@gmail.com>
2024-02-05 14:43:37 -0300

    Agregar validacion de tipo de variable

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -3,11 +3,16 @@ let edad ;
 do{
     edad = parseInt(prompt('Ingrese su edad: '));
 
-    if (edad >= 18 && edad <= 71) {
+    if (!isNaN(edad)){
+      if (edad >= 18 && edad <= 71) {
         alert("Puede adquirir un permiso para conducir");
       } else if (edad < 18) {
         alert("Sos menor de edad, no puedes tener un permiso para conducir");
       } else {
         alert("No puede manejar ");
       }
+    }else{
+      alert ("El numero ingresado no es valivo")
+    }
+    
 }while(confirm('Desea ingresa otra edad?'));
